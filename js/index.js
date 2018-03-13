2f9523a546965b04af7a74b62ab01a98de4ba440
jyery30120 <jyery30120@gmail.com>
2018-03-13 17:59:08 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 document.getElementsByTagName("h1")[0].style.fontSize = "80px";
+
+$(function(){
+	$("#switch").toggle(function(){
+		$("#demo").fadeOut("fast",function(){
+			$(this).fadeIn("fast").addClass("showblock");
+		});
+	},function(){
+		$("#demo").fadeOut("fast",function(){
+			$(this).fadeIn("fast").removeClass("showblock");
+		});
+	});
+});
